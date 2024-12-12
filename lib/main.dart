a07722575d98b23f7967d7d0e5c77d1f91a451e1
MahmoudMohamed447 <mahm2003100@gmail.com>
2024-12-12 16:44:42 +0200

    create commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatefulWidget {
+  const NotesApp({super.key});
+
+  @override
+  State<NotesApp> createState() => _NotesAppState();
+}
+
+class _NotesAppState extends State<NotesApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
